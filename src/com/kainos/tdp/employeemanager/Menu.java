fbe63d5b2e57a7949abdfbc6cf53b4f8bb843c25
Peter Lappin <peterl@kainos.com>
2018-10-23 14:18:07 +0100

    Added input methods for each value

--- src/com/kainos/tdp/employeemanager/Menu.java
+++ src/com/kainos/tdp/employeemanager/Menu.java
@@ -1,6 +1,8 @@
 package com.kainos.tdp.employeemanager;
+import java.util.Scanner;
 
 public class Menu {
+	public Scanner sc = new Scanner(System.in);
 	
 	public void printMenu() {
 		System.out.println("******************************");
@@ -20,4 +22,44 @@ public class Menu {
 			return true;
 		}
 	}
+	
+	public void addEmployee() {
+		String name = inputName();
+		String address = inputAddress();
+		String niNumber = inputNINumber();
+		String IBAN = inputIBAN();
+		String employeeNo = inputEmployeeNo();
+		float startingSalary = inputStartingSalary();
+	}
+
+	private String inputName() {
+		System.out.println("\nPlease Enter Employee Name : ");
+		return sc.nextLine();
+	}
+
+	private String inputAddress() {
+		System.out.println("\nPlease Enter Address : ");
+		return sc.nextLine();
+	}
+
+	private String inputNINumber() {
+		System.out.println("\nPlease Enter NI Number : ");
+		return sc.nextLine();
+	}
+	
+	private String inputIBAN() {
+		System.out.println("\nPlease Enter IBAN : ");
+		return sc.nextLine();
+	}
+	
+	private String inputEmployeeNo() {
+		System.out.println("\nPlease Enter Employee Number : ");
+		return sc.nextLine();
+	}
+	
+	private float inputStartingSalary() {
+		System.out.println("\nPlease Enter Starting Salary : ");
+		return sc.nextFloat();
+	}
+	
 }
