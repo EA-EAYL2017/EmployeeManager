107ab6134a12eda0b041c7c8c03be690efa4d9e9
Peter Lappin <peterl@kainos.com>
2018-10-23 13:15:50 +0100

    Added barebones menu

--- src/com/kainos/tdp/employeemanager/Main.java
+++ src/com/kainos/tdp/employeemanager/Main.java
@@ -4,6 +4,17 @@ import java.util.Scanner;
 public class Main {
 	
 	public static void main(String[] args) {
+		runMenu();
+	}
+	
+	private static void runMenu() {
+		int userInput;
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("******************************");
+		System.out.println("       Welcome to the         ");
+		System.out.println("     Employee Management      ");
+		System.out.println("           System             ");
+		System.out.println("******************************");
 	}
 }
