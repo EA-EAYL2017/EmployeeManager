4906fa96de0a1b0dd764cff72cd500d5a40462cc
Peter Gordon <p.gordon@kainos.com>
2018-10-26 16:11:49 +0100

    DB permission fix

--- db/setup.sh
+++ db/setup.sh
@@ -4,3 +4,6 @@ echo DB Setup Complete!
 if [ -f '02-add-department.sql' ]; then
 	sudo mysql -e "set @username='${DB_USER}';set @password='${DB_PASS}';source 02-add-department.sql;"
 fi
+if [ -f '03-grant-update-delete.sql' ]; then
+	sudo mysql -e "set @username='${DB_USER}';set @password='${DB_PASS}';source 03-grant-update-delete.sql;"
+fi
